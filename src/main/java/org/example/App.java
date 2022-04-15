71cdea5794b52be21a6fb70228760fb0c886e2a2
nguyenvule1112 <nguyenvule1112@gmail.com>
2022-04-15 11:23:08 +0700

    init commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+}}
---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,72 @@
+package org.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    WebDriver driver;
+    @Before
+    public void Beforetest(){
+        System.setProperty("webdriver.gecko.driver","C:\\Users\\admin\\Downloads\\geckodriver-v0.31.0-win64\\geckodriver.exe");
+        this.driver = new FirefoxDriver();
+        driver.manage().window().maximize();//phong to trinh duyet
+    }
+    @After
+    public void Aftertest(){
+        this.driver.quit();
+    }
+
+    @Test
+    public void DemoQA()
+    {
+        {
+            driver.get("https://demoqa.com/swagger/");
+        }
+    }
+
+    @Test
+    public void Getandprinttitle()
+    {
+        {
+            driver.get("https://demoqa.com/swagger/");
+            String title = driver.getTitle();
+            System.out.println("Page title :" +title);
+            System.out.println("Title length:" +title.length());
+
+        }
+    }
+
+    @Test
+    public void GetpageURL()
+    {
+        {
+            driver.get("https://demoqa.com/swagger/");
+            String url = driver.getCurrentUrl();
+            String title = driver.getTitle();
+            String titleexpect = "Swagger UI";
+
+            Assert.assertEquals(title,titleexpect);
+        }
+    }
+
+    @Test
+    public void GetPagesource()
+    {
+        {
+            driver.get("https://demoqa.com/swagger/");
+            String source = driver.getPageSource();
+            System.out.println("Source length:" +source.length());
+
+        }
+    }
+}
